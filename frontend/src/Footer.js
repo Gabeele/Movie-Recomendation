1c7fb1d342168c1418d1906994ebd9b7ffecd79d
Gavin Abeele <gabeele2160@conestogac.on.ca>
2022-12-27 21:43:57 +0000

    Updates to the foot

--- frontend/src/Footer.js
+++ frontend/src/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
     return (
         <div className="footer">
             <a href="https://github.com/Gabeele/Movie-Recomendation" target="_blank" rel="noreferrer noopener">Code</a>
-            <a href="https://github.com/Gabeele/Movie-Recomendation" target="_blank" rel="noreferrer noopener">How it works</a>
+            <a href="https://github.com/Gabeele/Movie-Recomendation/blob/main/README.md" target="_blank" rel="noreferrer noopener">How it works</a>
             <a href="https://www.gavinabeele.com" target="_blank" rel="noreferrer noopener">Socials</a>
         </div>
     )
