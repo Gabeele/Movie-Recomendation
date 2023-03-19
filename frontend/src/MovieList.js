6bf48a3eda2d41105afb7c336fa4f56f5c59cb26
sandeep-chahal <sandeepchahal573@gmail.com>
2023-03-18 20:08:04 -0400

    added loading animations reduces movie card size

--- frontend/src/MovieList.js
+++ frontend/src/MovieList.js
@@ -1,12 +1,28 @@
-import React from "react"
-import Movie from "./Movie"
+import React from "react";
+import Movie from "./Movie";
+import Loader from "./loader";
 
-export default function MovieList({movieList}) {
-    return (
-        movieList && movieList.movies.map(movie => {
-            return <div className="movie-card" key={movie.url}><Movie movie={movie} /></div>
-        })
-    )
-
-
+export default function MovieList({ movieList }) {
+	return (
+		<>
+			{movieList?.loading ? (
+				<div>
+					<Loader />
+				</div>
+			) : null}
+			{movieList?.isListEmpty ? (
+				<div className="no-movies">No movies found by this name!</div>
+			) : null}
+			{/* {!movieList?.isListEmpty && !movieList?.loading && !movieList?.movies?.length ? (
+				<div>Search any movie</div>
+			) : null} */}
+			{movieList?.movies?.map((movie) => {
+				return (
+					<div className="movie-card" key={movie.url}>
+						<Movie movie={movie} />
+					</div>
+				);
+			})}
+		</>
+	);
 }
